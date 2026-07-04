e436c453f9fa58a3a50be3e236c0ef43db3b5ec7
Sravani <s8858537@gmail.com>
2026-07-04 09:43:26 +0530

    Create array_intro.c

---
+++ array_intro.c
@@ -0,0 +1,17 @@
+
+
+
+int main() {
+    int marks[5] = {85, 90, 78, 92, 88};
+
+    printf("First mark: %d\n", marks[0]);
+    printf("Third mark: %d\n", marks[2]);
+
+    return 0;
+}
+
+/*
+Output:
+First mark: 85
+Third mark: 78
+*/
